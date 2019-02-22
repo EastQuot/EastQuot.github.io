56e42bce3cbc3827231b633cfa36cb83cac520b2
East <dyatlov.gleb@gmail.com>
2019-02-22 17:51:02 +0300

    init

---
+++ gym/app/js/common.js
@@ -0,0 +1,150 @@
+$(function () {
+
+	let qs = (selector) => document.querySelector(selector)
+	let qsAll = (selector) => document.querySelectorAll(selector)
+
+
+	let headerLine = qs('.header-line'),
+		topMenu = qs('.top-menu').cloneNode(true),
+		mobileMenuButton = qs('.mobile-menu-button');
+
+	let a = headerLine.insertAdjacentHTML('afterEnd', '<div class="mobile-menu d-lg-none"></div>')
+	let mobMenu = qs(".mobile-menu");
+
+	mobMenu.appendChild(topMenu)
+
+
+	$('.mobile-menu-button').click(function () {
+		$('.mobile-menu').stop().slideToggle();
+	})
+
+
+
+
+
+	function Menu(options) {
+		var elem = options.elem;
+
+		elem.forEach(function (elem) {
+			elem.onmousedown = function () {
+				return false;
+			}
+
+			elem.onclick = function (event) {
+				event.stopPropagation();
+				event.preventDefault();
+				elem.classList.toggle('open');
+
+				// function open(e){
+				// 	console.log(1)
+				// 	elem.classList.remove('open')
+				// 	document.removeEventListener('click',open)
+				// }
+
+				// document.addEventListener('click', open)
+
+			};
+			elem.onmouseleave = function (event) {
+				elem.classList.remove('open')
+			}
+		})
+
+	}
+
+	Menu({ elem: qsAll('.classes') });
+
+
+	let likeTrainers = qs('.our-trainers');
+
+	likeTrainers.onclick = function (e) {
+		if (e.target.className == 'fa fa-heart') {
+			e.preventDefault();
+			e.target.style.color = e.target.style.color == 'red' ? '' : 'red';
+		}
+		if (e.target.className == 'fa fa-thumbs-up') {
+			e.preventDefault();
+			e.target.style.color = e.target.style.color == 'rgb(2, 158, 198)' ? '' : 'rgb(2, 158, 198)';
+		}
+	}
+
+
+	$(document).ready(function () {
+
+		$('.our-trainers').owlCarousel({
+			// loop:true,
+			pagination: false,
+			nav: true,
+			items: 2,
+			navText: "",
+			responsiveClass: true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				400: {
+					items: 2
+				},
+				768: {
+					items: 3
+				},
+				992: {
+					items: 2
+				}
+			}
+		})
+
+		$('.gallery').owlCarousel({
+			// loop:true,
+			pagination: false,
+			nav: true,
+			items: 1,
+			navText: "",
+			responsiveClass: true,
+			responsive: {
+				0: {
+					items: 0
+				},
+				400: {
+					items: 1
+				},
+				768: {
+					items: 1
+				},
+				992: {
+					items: 1
+				}
+			}
+		})
+
+
+
+	});
+
+	$('.footer-menu h4').click(function () {
+		$('.footer-menu ul').stop().slideToggle();
+	})
+	$('.community h4').click(function () {
+		$('.community ul').stop().slideToggle();
+	})
+
+
+	$(window).on('load', function () {
+		$('.preloader').fadeOut('slow');
+	})
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ gym/gulpfile.js
@@ -0,0 +1,130 @@
+let s        = 'sass', // Syntax: sass or scss;
+
+	  gulp          = require('gulp'),
+		gutil         = require('gulp-util' ),
+		sass          = require('gulp-sass'),
+		browserSync   = require('browser-sync'),
+		concat        = require('gulp-concat'),
+		uglify        = require('gulp-uglify'),
+		cleancss      = require('gulp-clean-css'),
+		rename        = require('gulp-rename'),
+		del           = require('del'),
+		autoprefixer  = require('gulp-autoprefixer'),
+		notify        = require('gulp-notify'),
+		imagemin      = require('gulp-imagemin'),
+		pngquant      = require('imagemin-pngquant'),
+		cache         = require('gulp-cache'), 
+		rsync         = require('gulp-rsync');
+
+gulp.task('browser-sync', function() {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		open: false,
+		// online: false, // Work Offline Without Internet Connection
+		// tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+
+gulp.task('styles', function() {
+	return gulp.src('app/'+s+'/**/*.'+s+'')
+	.pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+	.pipe(rename({ suffix: '.min', prefix : '' }))
+	.pipe(autoprefixer(['last 15 versions']))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/OwlCarousel/owl.carousel.min.js',
+		'app/js/common.js', // Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	// .pipe(uglify()) // Mifify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+
+gulp.task('watch', function() {
+		gulp.watch('app/'+s+'/**/*.'+s+'', gulp.parallel('styles'));
+		gulp.watch(['libs/**/*.js', 'app/js/common.js'], gulp.parallel('scripts'));
+		gulp.watch('app/*.html', gulp.parallel('code'))
+	});
+
+
+
+
+
+	gulp.task('clean', async function() {
+		return del.sync('dist'); // Удаляем папку dist перед сборкой
+	});
+	
+	gulp.task('img', function() {
+		return gulp.src('app/img/**/*') // Берем все изображения из app
+			.pipe(cache(imagemin({ // С кешированием
+			// .pipe(imagemin({ // Сжимаем изображения без кеширования
+				interlaced: true,
+				progressive: true,
+				svgoPlugins: [{removeViewBox: false}],
+				use: [pngquant()]
+			}))/**/)
+			.pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+	});
+	
+	gulp.task('prebuild', async function() {
+	
+		var buildCss = gulp.src([ // Переносим библиотеки в продакшен
+			'app/css/main.min.css',
+			])
+		.pipe(gulp.dest('dist/css'))
+	
+		var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+		.pipe(gulp.dest('dist/fonts'))
+	
+		var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+		.pipe(gulp.dest('dist/js'))
+	
+		var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+		.pipe(gulp.dest('dist'));
+	
+	});
+
+
+
+
+
+	gulp.task('default', gulp.parallel('styles', 'scripts', 'browser-sync', 'watch'));
+	gulp.task('build', gulp.parallel('prebuild', 'clean', 'img', 'styles', 'scripts'));
+
+
+
+
+
+
+	
+		
+		gulp.task('rsync', function() {
+			return gulp.src('app/**')
+			.pipe(rsync({
+				root: 'app/',
+				hostname: 'username@yousite.com',
+				destination: 'yousite/public_html/',
+				// include: ['*.htaccess'], // Includes files to deploy
+				exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+				recursive: true,
+				archive: true,
+				silent: false,
+				compress: true
+			}))
+		});
